294a277047926d871c46ee6aab838e144d644dc6
tangchengxiang <2064027004@qq.com>
2026-08-09 01:17:34 +0000

    complete homework 3 clip shape inference

--- src/operators/unary.cc
+++ src/operators/unary.cc
@@ -39,7 +39,8 @@ namespace infini
         // TODO：返回经过 clip 操作后的 shape
         // REF: https://onnx.ai/onnx/operators/onnx__Clip.html#clip-13
         // =================================== 作业 ===================================
-        return std::nullopt;
+        // Clip is element-wise, so it preserves the input shape.
+        return {{inputs[0]->getDims()}};
     }
 
     std::string ClipObj::toString() const
